a93780c990e9b6269fb92d460c89ef912161c146
noumecha spaker <noumechaivan@gmail.com>
2022-09-30 13:26:38 +0100

    maj: by tmc

--- wbu-atomique-theme/src/js/carrouselnav.js
+++ wbu-atomique-theme/src/js/carrouselnav.js
@@ -1,2 +1,2 @@
-import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/mitor/js/mitor-derived-header.js";
+//import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/mitor/js/mitor-derived-header.js";
 import "@stephane888/wbu-atomique/js/swiper/swiper-drupal.js";
